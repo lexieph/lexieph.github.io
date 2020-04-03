ecb2fa683249f66ad58320d697baa1fc895faf1f
lexieph <gtmrit@gmail.com>
2020-04-03 09:29:49 +0800

    Added content protection
    Disabled right-click
    Disabled copy-paste

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.onload = init;
 
+/* TOGGLE BUTTON from BoogieJack.com*/
+
 function init() {
 	openNav();
 }
@@ -19,3 +21,45 @@ function openNav() {
 function closeNav() {
     document.getElementById("post_toc").style.width = "0";
 }
+
+/*DISABLE COPY PASTE*/
+
+function killCopy(e){
+	return false;
+}
+
+function reEnable(){
+	return true;
+}
+
+document.onselectstart = new Function ("return false")
+if (window.sidebar) {
+	document.onmousedown = killCopy;
+	document.onclick = reEnable
+}
+
+/*DISABLE RIGHT-CLICK from BoogieJack.com*/
+var message = "Right Click Disabled"
+
+function defeatIE() {
+	if(document.all){(message);return false;}
+}
+
+function defeatNS(e) {
+	if(document.layers || (document.getElementById&&!document.all)) {
+		if (e.which==2 || e.which==3) {(message); return false;}
+	}
+}
+
+if(document.layers) {
+	document.captureEvents(Event.MOUSEDOWN);
+	document.onmousedown = defeatNS;
+}
+
+else {
+	document.onmouseup = defeatNS;
+	document.oncontextmenu = defeatIE;
+}
+
+document.oncontextmenu = new Function("return false");
+
